28efea650242f6b6c7c56d24f1dfff6615781d53
chenming0416 <cjmcjh1118>
2020-05-08 18:22:26 +0800

    release分支第二次提交

--- src/main/java/com/example/demo/controller/HomeController.java
+++ src/main/java/com/example/demo/controller/HomeController.java
@@ -11,6 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class HomeController {
     @RequestMapping("/")
     public String home(){
-        return "Hello,这是release分支的demo!是基于feature分支来的";
+        return "Hello,这是release分支的demo!release第二次提交";
     }
 }
